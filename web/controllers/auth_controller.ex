8cc5a241c35c617c88713739b1135a06344a7d35
karn09 <karn09@gmail.com>
2018-02-19 14:43:55 -0500

    adds signin with redirect to index

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -7,10 +7,24 @@ defmodule Discuss.AuthController do
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
 
-    changeset =
-      User.changeset(%User{}, user_params)
-      |> insert_or_update_user
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:error, "Welcome.")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end
   end
 
   defp insert_or_update_user(changeset) do
