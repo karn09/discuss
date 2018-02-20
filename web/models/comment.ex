d7c0a7ce42916b012c7c9c513cf3de9ce33716eb
karn09 <karn09@gmail.com>
2018-02-19 23:39:02 -0500

    specify fields to encode to json

--- web/models/comment.ex
+++ web/models/comment.ex
@@ -1,6 +1,9 @@
 defmodule Discuss.Comment do
   use Discuss.Web, :model
 
+  # Specify that Poison only encode to JSON specified atoms.
+  @derive {Poison.Encoder, only: [:content]}
+
   schema "comments" do
     field(:content, :string)
     belongs_to(:user, Discuss.User)
