94dca4d0868335394bcd07a16c989c2b49a0c5df
karn09 <karn09@gmail.com>
2018-02-19 23:01:03 -0500

    handle create socket comment.

---
+++ web/channels/comments_channel.ex
@@ -0,0 +1,32 @@
+defmodule Discuss.CommentsChannel do
+  use Discuss.Web, :channel
+
+  alias Discuss.{Topic, Comment}
+
+  def join("comments:" <> topic_id, _params, socket) do
+    # pattern match topic_id (as str) from "comments:6"
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, assign(socket, :topic, topic)}
+  end
+
+  def handle_in(name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset =
+      topic
+      |> build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, comment} ->
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+
+    {:reply, :ok, socket}
+  end
+end
