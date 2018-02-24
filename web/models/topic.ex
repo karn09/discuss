95fa165bcf60bdc4ee6599d340f46dc2baabc1ed
karn09 <karn09@gmail.com>
2018-02-24 16:49:12 -0500

    Fixes topic belongs to user reference.

--- web/models/topic.ex
+++ web/models/topic.ex
@@ -3,7 +3,7 @@ defmodule Discuss.Topic do
 
   schema "topics" do
     field(:title, :string)
-    belongs_to(:user, Discuss.Topic)
+    belongs_to(:user, Discuss.User)
     has_many(:comments, Discuss.Comment)
   end
 
